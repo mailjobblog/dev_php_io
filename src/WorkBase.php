6c0c25d3c0e993913dc3167955a91d8d5e57559f
jefferyjob <58850169@qq.com>
2021-04-01 22:19:17 +0800

    push

--- src/WorkBase.php
+++ src/WorkBase.php
@@ -17,6 +17,8 @@ abstract class WorkBase {
 
     protected $config;
 
+    protected $type = 'tcp';
+
     public function __construct($host, $port){
         $this->server = stream_socket_server("tcp://{$host}:{$port}");
         echo "tcp://{$host}:{$port}\n";
@@ -61,6 +63,37 @@ abstract class WorkBase {
      */
     public function start(){
         dd($this->events,'服务注册的事件');
-        $this->accept();
+        $this->check();// 检查启动信息
+        $this->accept(); // 启动
+    }
+
+    /**
+     * 用于校验服务启动信息
+     * 
+     * 校验是否注册事件，以及注册的事件类型
+     */
+    public function check()
+    {
+        if ($this->type == 'tcp') {
+            if (empty($this->event['connect']) || !$this->event['connect'] instanceof Closure) {
+                dd("tcp服务必须要有回调事件: connect");
+                exit;
+            }
+
+            if (empty($this->event['receive']) || !$this->event['receive']  instanceof Closure ) {
+                dd("tcp服务必须要有回调事件: receive");
+                exit;
+            }
+
+            if (empty($this->event['close']) || !$this->event['close'] instanceof Closure ) {
+                dd("tcp服务必须要有回调事件: close");
+                exit;
+            }
+        } else if ($this->type == 'http') {
+            if (empty($this->event['request']) || !$this->event['request'] instanceof Closure ) {
+                dd("http服务必须要有回调事件: request");
+                exit;
+            }
+        }
     }
 }
