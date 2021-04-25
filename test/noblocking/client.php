13abcbff46267eb266ece25516baf22aab36c6d9
jefferyjob <58850169@qq.com>
2021-04-25 22:59:21 +0800

    php-io非阻塞模型

--- test/noblocking/client.php
+++ test/noblocking/client.php
@@ -6,19 +6,16 @@ $fp = stream_socket_client("tcp://127.0.0.1:9500");
 //设置套接字为非阻塞模型
 stream_set_blocking($client, 0);
 
-fwrite($fp,'hello blocking');
+fwrite($fp,'hello NO-blocking');
 
 $time = time();
 
 echo fread($fp,65535);
 
-echo "\n其他业务\r\n";
+echo "\n此处执行其他业务代码\r\n";
 
 $m = time() - $time;
 
 echo "执行时间" . $m . "秒钟\n";
 
-//不断轮询，有内容则打印
-while (!feof($client)) {
-  echo fread($client, 65535);
-}
+fclose($fp);
