17411097d697ddf9538debd1c1e045aa38db21fe
libin <bin.li@gaea.com>
2021-04-30 17:27:56 +0800

    push

--- test/multiplexing/server.php
+++ test/multiplexing/server.php
@@ -1,7 +1,7 @@
 <?php
 require_once __DIR__."/../../vendor/autoload.php";
 
-use IoStar\Muliplexing\Worker;
+use DevPhpIO\Multiplexing\Worker;
 
 $server = new Worker('0.0.0.0', 9500);
 $server->on('connect', function($server, $client){
